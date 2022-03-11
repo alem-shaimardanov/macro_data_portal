fa33d05da7f2ce117970786df4bcc7f34371a1ee
Alem <ans682@nyu.edu>
2022-03-11 12:41:00 +0600

    updated workflow for converting minfin to closure tables

--- parsers/minfin_insert_in_closure_table.py
+++ parsers/minfin_insert_in_closure_table.py
@@ -26,43 +26,40 @@ v_tom_chisle = "в том числе"
 
 
 try:
-    # Insert month into 'posts' table
-    cur.execute("INSERT INTO posts (content) VALUES ('" + indicator_name + "')")
+    ######### Pre-work ##########
+    # # Insert month into 'posts' table
+    # cur.execute("INSERT INTO posts (content) VALUES ('" + indicator_name + "')")
     
-    # Retrieve post_id of a newly added post from the 'posts' table
-    cur.execute("SELECT postid FROM posts WHERE content = '" + indicator_name + "'")
-    records = cur.fetchone()
-    post_id = records[0]
-    print("Post_id: ",post_id)
+    # # Retrieve post_id of a newly added post from the 'posts' table
+    # cur.execute("SELECT postid FROM posts WHERE content = '" + indicator_name + "'")
+    # records = cur.fetchone()
+    # post_id = records[0]
+    # print("Post_id: ",post_id)
 
-    # ////////
-    # Insert month into 'comments_data' table
-    cur.execute("INSERT INTO comments_data (content, post_id) VALUES ('" + month_name + "','" + str(post_id) + "')")
+    # # ////////
+    # # Insert month into 'comments_data' table
+    # cur.execute("INSERT INTO comments_data (content, post_id) VALUES ('" + month_name + "','" + str(post_id) + "')")
     
-    # Retrieve idEntry of a newly added comment from the 'comments_data' table
-    cur.execute("SELECT idEntry FROM comments_data WHERE content = '" + month_name + "'")
-    records = cur.fetchone()
-    comment_id = records[0]
-    print("Comment_id: ",post_id)
+    # # Retrieve idEntry of a newly added comment from the 'comments_data' table
+    # cur.execute("SELECT idEntry FROM comments_data WHERE content = '" + month_name + "'")
+    # records = cur.fetchone()
+    # comment_id = records[0]
+    # print("Comment_id: ",post_id)
 
-    # ////////
-    # Insert comment_id into 'comments_tree' table
-    values_to_be_inserted = str(comment_id) + "," + str(comment_id) + "," +"0,0," + str(post_id)
-    print("VALUES: ", values_to_be_inserted)
-    # cur.execute("INSERT INTO comments_tree (idAncestor, idDescendant, idNearestAncestor, commentLevel, idSubject) VALUES ('" + str(comment_id) + "','" + str(comment_id) + "','0','0','" + str(post_id) + "')")
-    cur.execute("INSERT INTO comments_tree (idAncestor, idDescendant, idNearestAncestor, commentLevel, idSubject) VALUES (" + values_to_be_inserted + ")")
+    # # Insert comment_id into 'comments_tree' table
+    # cur.execute("INSERT INTO comments_tree (idAncestor, idDescendant, idNearestAncestor, commentLevel, idSubject) VALUES (" + str(comment_id) + "," + str(comment_id) + "," +"0,0," + str(post_id) + ")")
     
-    print("INSERTED successfully into comments_tree ...")
 
-    # Retrieve idAncestor, idNearestAncestor, commentLevel of a newly added row from the 'comments_tree' table
-    cur.execute("SELECT idAncestor, idNearestAncestor, commentLevel FROM comments_tree WHERE idDescendant = '" + str(comment_id) + "'")
-    records = cur.fetchall()
-    idAncestor = records[0][0]
-    idNearestAncestor = records[0][1]
-    commentLevel = records[0][1]
-    print("idAncestor: ",idAncestor)
-    print("idNearestAncestor: ",idNearestAncestor)
-    print("commentLevel: ",commentLevel)
+    # # Retrieve idAncestor, idNearestAncestor, commentLevel of a newly added row from the 'comments_tree' table
+    # cur.execute("SELECT idAncestor, idNearestAncestor, commentLevel FROM comments_tree WHERE idDescendant = '" + str(comment_id) + "'")
+    # records = cur.fetchall()
+    # idAncestor = records[0][0]
+    # idNearestAncestor = records[0][1]
+    # commentLevel = records[0][1]
+    # print("idAncestor: ",idAncestor)
+    # print("idNearestAncestor: ",idNearestAncestor)
+    # print("commentLevel: ",commentLevel)
+
 
     v_tom_chisle_gen1 = False
     v_tom_chisle_gen2 = False
@@ -71,14 +68,37 @@ try:
     # Iterate through the json list:
     for item in json_object:
 
-        print("Type of item: ", type(item))
-        print("ITEM: ", item, " ; VALUE: ", json_object[item])
-        # if v_tom_chisle in item:
-        #     print("V_tom_chisle: ", json_object[item])
+        # print("Type of item: ", type(item))
+        # print("ITEM: ", item, " ; VALUE: ", json_object[item])
+
+        # Check if the current item is a main category
+        if v_tom_chisle not in item and v_tom_chisle_gen1 == False and v_tom_chisle_gen2 == False:
+            print("V_tom_chisle: ", json_object[item])
+            # Insert subcom to 'month' main comment
+
+        # Check if the current item is a dict of main category
+        elif v_tom_chisle in item and v_tom_chisle_gen1 == False and v_tom_chisle_gen2 == False:
+            v_tom_chisle_gen1 = True
+            print("Inside dashed strings")
+            for elem in json_object[item]:
+                print("Dashed string: ", elem)
+                if v_tom_chisle in elem:
+                    print("Found v_tom_chisle_gen2: ", elem)
+                    v_tom_chisle_gen2 = True
+                    for sub_elem in json_object[item][elem]:
+                        print("subcom 2: ", sub_elem)
+            
+        
+            v_tom_chisle_gen1 = False
+
+
+        # Check if the current item is a dashed string
+        
+
         # if isinstance(item, dict):
         #     for k, v in item.items():
         #         print ("SUBDICT. Key: ", k, " ; Value: ", v)
-        print("------------------------------------------------------")
+        
 
         '''
         Before:
