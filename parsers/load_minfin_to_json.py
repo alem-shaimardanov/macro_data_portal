46b325e62693e10e61cc575636d526c0d7920556
Alem <ans682@nyu.edu>
2022-03-11 09:51:28 +0600

    checked conversion of minfin excel to json

--- parsers/load_minfin_to_json.py
+++ parsers/load_minfin_to_json.py
@@ -158,8 +158,8 @@ for ind in excel_data_df.index:
 print("Final JSON:")
 print(excel_json_gen0)
 
-# # Save dict in json
-# indicator_name = "ОТЧЕТ О ПОСТУПЛЕНИЯХ И ИСПОЛЬЗОВАНИИ НАЦИОНАЛЬНОГО ФОНДА РЕСПУБЛИКИ КАЗАХСТАН"
-# json_file_name = indicator_name + ".json"
-# with open(json_file_name, 'w') as json_file:
-#     json.dump(excel_json_gen0, json_file)
+# Save dict in json
+indicator_name = "ОТЧЕТ О ПОСТУПЛЕНИЯХ И ИСПОЛЬЗОВАНИИ НАЦИОНАЛЬНОГО ФОНДА РЕСПУБЛИКИ КАЗАХСТАН"
+json_file_name = indicator_name + ".json"
+with open(json_file_name, 'w') as json_file:
+    json.dump(excel_json_gen0, json_file)
--- parsers/minfin_insert_in_closure_table.py
+++ parsers/minfin_insert_in_closure_table.py
@@ -29,73 +29,22 @@ try:
         print("ITEM: ", item)
 
         print("------------------------------------------------------")
-        # if i < 1:
-        #     cur_term_items_list = item['termNames']
-        #     term_item_group_id = "0"
-        #     ### CHECK IF term_item_group_id EXISTS. if exists, save it; if not, then set term_item_group_id = "1"
-        #     cur.execute("SELECT * FROM term_item_groups ORDER BY term_item_group_id DESC LIMIT 1")
-        #     records = cur.fetchall()
-        #     if len(records) == 0:
-        #         # term_item_group_id = "1"
-        #         pass
-        #     else:
-        #         term_item_group_id = str(records[0][2])
+
+        '''
+        Before:
+        1. Insert 'otchet o post...' to 'posts' table. Return its post_id.
+        2. Insert 'feb 2022' to 'comments_data' table. Return its comment_id.
+        3. Insert into 'comments_tree' (idAncestor, idDescendant, idNearestAncestor, commentLevel, post_id)
+            values (comment_id, comment_id, 0, 0, post_id).
+        
+        Logic:
+        1. if main key found (v_tom_chisle not in item_name and v_tom_chisle_gen1 == False and v_tom_chisle_gen2 == False):
             
-        #     term_item_group_id_iterator = 0
+        2. elif 
+        '''
 
-        #     for term_item_name in cur_term_items_list:
-        #         # Check if term_item_name is already in the table 'term_item_names'
-        #         cur.execute("SELECT COUNT(1) FROM term_item_names WHERE term_item_name='" + term_item_name + "')")
-        #         records = cur.fetchall()
-        #         term_group_id = "0"
 
-        #         # If term_item_name is not in the table
-        #         if records[0][0] == 0:
-
-        #             # Insert term_item_name into 'term_item_names' table
-        #             cur.execute("INSERT INTO term_item_names (term_item_name) VALUES ('" + term_item_name + "')")
-                
-        #         if term_item_group_id_iterator % 2 == 0:
-        #             term_item_group_id_integer = int(term_item_group_id)
-        #             term_item_group_id_integer += 1
-        #             term_item_group_id = str(term_item_group_id_integer)
-        #         else:
-        #             pass
-                
-        #         # Fetch 'term_item_name_id' from 'term_item_names' table
-        #         cur.execute("SELECT term_item_name_id FROM term_item_names where term_item_name='" + term_item_name + "')")
-        #         records = cur.fetchall()
-        #         term_item_id = records[0][0]
-                
-            
-        #     # Insert 'term_category_group_id', 'term_item_group_id', 'term_item_id' into 'term_item_groups' table
-        #     cur.execute("INSERT INTO term_item_groups (term_category_group_id, term_item_group_id, term_item_id) VALUES ('" +
-        #     "','" +  current_term_category_group_id + "','"  + term_item_group_id + "','" + term_item_id + "')")
-            
-        #     # Insert 'term_item_group_id', 'indic_period_id' into 'term_item_group_indic_periods' table
-
-            
-        #     cur_period_list = item['periods']
-        #     for elem in cur_period_list:
-        #         elem_name = elem["name"]
-        #         elem_date = elem["date"]
-        #         elem_value_string = elem["value"].replace(',','.') # replace all commas with dots
-        #         elem_value_long = 0.0
-        #         if elem_value_string == "" or not regnumber.match(elem_value_string): # use regular expressions
-        #             elem_value_string = "NULL"
-        #             elem_value_long = "NULL"
-        #         else:
-        #             elem_value_long = float(elem_value_string)
-                    
-        #         print("Name: ", elem_name, ", Date: ", elem_date, ", Value: ", elem_value_string)
-
-        #         # Insert 'value_id', 'termName_term_id', 'name', 'date', 'value_string', 'value_long', 'date_created'
-        #         cur.execute("INSERT INTO taldau_values (termName_term_id, name, date, value_string, value_long, date_created) VALUES('" + str(termName_term_id) + "','" + elem_name + \
-        #         "','" + elem_date +  "','" + elem_value_string + "','" + str(elem_value_long) + "','" + str(date_today) + "')")
-                
-                
-
-        # i += 1
+        
 except:
     con.rollback()
 # -----------------------------------------------
