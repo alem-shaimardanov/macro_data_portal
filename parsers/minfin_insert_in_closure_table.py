9d26814bd6ee60676ebd83f82ffbb8548dfbb9fa
Alem <ans682@nyu.edu>
2022-03-11 11:32:20 +0600

    added insertion of indicator_name into closure table

--- parsers/minfin_insert_in_closure_table.py
+++ parsers/minfin_insert_in_closure_table.py
@@ -1,7 +1,9 @@
+from calendar import month
 import sqlite3
 import json
 indicator_name = "ОТЧЕТ О ПОСТУПЛЕНИЯХ И ИСПОЛЬЗОВАНИИ НАЦИОНАЛЬНОГО ФОНДА РЕСПУБЛИКИ КАЗАХСТАН"
 post_id = '1'
+month_name = "1 ФЕВРАЛЯ 2022 ГОДА "
 
 db_file = 'taldau_indicator1.db'
 
@@ -20,14 +22,62 @@ json_file = open(indicator_name + '.json')
 json_object = json.load(json_file)
 
 print("Size of json: ", len(json_object))
-# Iterate through the json list:
+v_tom_chisle = "в том числе"
+
 
 try:
-    i = 0
-    for item in json_object:
-        print("Type of item: ", type(item))
-        print("ITEM: ", item)
+    # Insert month into 'posts' table
+    cur.execute("INSERT INTO posts (content) VALUES ('" + indicator_name + "')")
+    
+    # Retrieve post_id of a newly added post from the 'posts' table
+    cur.execute("SELECT postid FROM posts WHERE content = '" + indicator_name + "'")
+    records = cur.fetchone()
+    post_id = records[0]
+    print("Post_id: ",post_id)
 
+    # ////////
+    # Insert month into 'comments_data' table
+    cur.execute("INSERT INTO comments_data (content, post_id) VALUES ('" + month_name + "','" + str(post_id) + "')")
+    
+    # Retrieve idEntry of a newly added comment from the 'comments_data' table
+    cur.execute("SELECT idEntry FROM comments_data WHERE content = '" + month_name + "'")
+    records = cur.fetchone()
+    comment_id = records[0]
+    print("Comment_id: ",post_id)
+
+    # ////////
+    # Insert comment_id into 'comments_tree' table
+    values_to_be_inserted = str(comment_id) + "," + str(comment_id) + "," +"0,0," + str(post_id)
+    print("VALUES: ", values_to_be_inserted)
+    # cur.execute("INSERT INTO comments_tree (idAncestor, idDescendant, idNearestAncestor, commentLevel, idSubject) VALUES ('" + str(comment_id) + "','" + str(comment_id) + "','0','0','" + str(post_id) + "')")
+    cur.execute("INSERT INTO comments_tree (idAncestor, idDescendant, idNearestAncestor, commentLevel, idSubject) VALUES (" + values_to_be_inserted + ")")
+    
+    print("INSERTED successfully into comments_tree ...")
+
+    # Retrieve idAncestor, idNearestAncestor, commentLevel of a newly added row from the 'comments_tree' table
+    cur.execute("SELECT idAncestor, idNearestAncestor, commentLevel FROM comments_tree WHERE idDescendant = '" + str(comment_id) + "'")
+    records = cur.fetchall()
+    idAncestor = records[0][0]
+    idNearestAncestor = records[0][1]
+    commentLevel = records[0][1]
+    print("idAncestor: ",idAncestor)
+    print("idNearestAncestor: ",idNearestAncestor)
+    print("commentLevel: ",commentLevel)
+
+    v_tom_chisle_gen1 = False
+    v_tom_chisle_gen2 = False
+
+    print("Entering for loop ...")
+    # Iterate through the json list:
+    for item in json_object:
+
+        print("Type of item: ", type(item))
+        print("ITEM: ", item, " ; VALUE: ", json_object[item])
+        # if v_tom_chisle in item:
+        #     print("V_tom_chisle: ", json_object[item])
+        # if isinstance(item, dict):
+        #     for k, v in item.items():
+        #         print ("SUBDICT. Key: ", k, " ; Value: ", v)
         print("------------------------------------------------------")
 
         '''
@@ -46,6 +96,7 @@ try:
 
         
 except:
+    print("ERROR")
     con.rollback()
 # -----------------------------------------------
 
