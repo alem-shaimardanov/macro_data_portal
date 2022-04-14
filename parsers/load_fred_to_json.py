651b21eb5b5f0054ada93f78b8521b2a98747e33
Alem <ans682@nyu.edu>
2022-04-14 16:58:22 +0600

    parsed 10 indicators from FRED

--- parsers/load_fred_to_json.py
+++ parsers/load_fred_to_json.py
@@ -6,24 +6,35 @@ from random import randint
 
 from django.template import base
 
-base_url_1 = "https://api.stlouisfed.org/fred/series/observations?series_id=GNPCA&api_key=7d1fa8e45a6f8b1fa966454d53fb91dd&file_type=json"
+base_url_1 = "https://api.stlouisfed.org/fred/series/observations?series_id=DDOE01KZA086NWDB&api_key=7d1fa8e45a6f8b1fa966454d53fb91dd&file_type=json"
 base_url_2 = "https://api.stlouisfed.org/fred/series/observations?series_id=CPIFABSL&api_key=7d1fa8e45a6f8b1fa966454d53fb91dd&file_type=json"
 base_url_3 = "https://api.stlouisfed.org/fred/series/observations?series_id=PCAGDPKZA646NWDB&api_key=7d1fa8e45a6f8b1fa966454d53fb91dd&file_type=json"
 base_url_4 = "https://api.stlouisfed.org/fred/series/observations?series_id=MKTGDPKZA646NWDB&api_key=7d1fa8e45a6f8b1fa966454d53fb91dd&file_type=json"
 base_url_5 = "https://api.stlouisfed.org/fred/series/observations?series_id=KAZNGDPDUSD&api_key=7d1fa8e45a6f8b1fa966454d53fb91dd&file_type=json"
 base_url_6 = "https://api.stlouisfed.org/fred/series/observations?series_id=KAZNGDPRPCHPT&api_key=7d1fa8e45a6f8b1fa966454d53fb91dd&file_type=json"
-urls_list = [base_url_1, base_url_2, base_url_3, base_url_4, base_url_5, base_url_6]
+base_url_7 = "https://api.stlouisfed.org/fred/series/observations?series_id=USACPIALLMINMEI&api_key=7d1fa8e45a6f8b1fa966454d53fb91dd&file_type=json"
+base_url_8 = "https://api.stlouisfed.org/fred/series/observations?series_id=FPCPITOTLZGUSA&api_key=7d1fa8e45a6f8b1fa966454d53fb91dd&file_type=json"
+base_url_9 = "https://api.stlouisfed.org/fred/series/observations?series_id=FPCPITOTLZGKAZ&api_key=7d1fa8e45a6f8b1fa966454d53fb91dd&file_type=json"
+base_url_10 = "https://api.stlouisfed.org/fred/series/observations?series_id=GDP&api_key=7d1fa8e45a6f8b1fa966454d53fb91dd&file_type=json"
 
-indicator_name_1 = "GNPCA"
-indicator_name_2 = "CPIFABSL"
+urls_list = [base_url_1, base_url_2, base_url_3, base_url_4, base_url_5, 
+    base_url_6, base_url_7, base_url_8, base_url_9, base_url_10]
+
+indicator_name_1 = "CPI|Kazakhstan"
+indicator_name_2 = "CPI FABSL|United States"
 indicator_name_3 = "GDP per capita|Kazakhstan"
 indicator_name_4 = "GDP|Kazakhstan"
 indicator_name_5 = "GDP in current prices|Kazakhstan"
 indicator_name_6 = "GDP in constant prices|Kazakhstan"
+indicator_name_7 = "CPI for all items|United States"
+indicator_name_8 = "Inflation Consumer Prices|United States"
+indicator_name_9 = "Inflation Consumer Prices|Kazakhstan"
+indicator_name_10 = "GDP|United States"
 
 
 
-indicator_names_list = [indicator_name_1, indicator_name_2, indicator_name_3, indicator_name_4, indicator_name_5, indicator_name_6]
+indicator_names_list = [indicator_name_1, indicator_name_2, indicator_name_3, indicator_name_4, indicator_name_5, 
+    indicator_name_6, indicator_name_7, indicator_name_8, indicator_name_9, indicator_name_10]
 
 ############## CODE TO LOAD JSON FROM FRED #################
 for i in range(len(urls_list)):
