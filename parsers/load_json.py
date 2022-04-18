631fa6ee4f8f4db719d20880f523ba276da33c5a
Alem <ans682@nyu.edu>
2022-04-18 17:16:32 +0600

    parsed 3 indicators with different categories from Taldau

--- parsers/load_json.py
+++ parsers/load_json.py
@@ -4,14 +4,14 @@ from urllib.request import urlopen
 from time import *
 from random import randint
 
-base_url_1 = "https://taldau.stat.gov.kz/ru/Api/GetIndexData/704449?period=7&dics=67"
-base_url_2 = "https://taldau.stat.gov.kz/ru/Api/GetIndexData/703088?period=5&dics=67,3030"
-base_url_3 = "https://taldau.stat.gov.kz/ru/Api/GetIndexData/703089?period=5&dics=68,481,1336,2493"
-base_url_4 = "https://taldau.stat.gov.kz/ru/Api/GetIndexData/703090?period=5&dics=68,524,2819"
-base_url_5 = "https://taldau.stat.gov.kz/ru/Api/GetIndexData/703094?period=4&dics=68,2513,2853"
+base_url_1 = "https://taldau.stat.gov.kz/ru/Api/GetIndexData/704449?period=7&dics=67" # 1 category
+base_url_2 = "https://taldau.stat.gov.kz/ru/Api/GetIndexData/703088?period=5&dics=67,3030" # 2 categories
+base_url_3 = "https://taldau.stat.gov.kz/ru/Api/GetIndexData/703089?period=5&dics=68,481,1336" # 3 categories
+base_url_4 = "https://taldau.stat.gov.kz/ru/Api/GetIndexData/703090?period=5&dics=68,524,2819" # 3 categories
+base_url_5 = "https://taldau.stat.gov.kz/ru/Api/GetIndexData/703094?period=4&dics=68,2513,2853" # 3 categories
 
 
-urls_list = [base_url_1, base_url_2, base_url_3, base_url_4, base_url_5]
+urls_list = [base_url_1, base_url_2, base_url_3]
 
 indicator_name_1 = "индекс реальных денежных доходов"
 indicator_name_2 = "Цены производителей на продукцию сельского хозяйства"
@@ -19,7 +19,7 @@ indicator_name_3 = "Цены на продукцию лесного хозяйс
 indicator_name_4 = "Цены на продукцию рыболовства и рыбоводства"
 indicator_name_5 = "Цены предприятий-производителей на промышленную продукцию"
 
-indicator_names_list = [indicator_name_1, indicator_name_2,indicator_name_3,indicator_name_4,indicator_name_5]
+indicator_names_list = [indicator_name_1, indicator_name_2,indicator_name_3]
 
 ############## CODE TO LOAD JSON FROM TALDAU #################
 for i in range(len(urls_list)):
